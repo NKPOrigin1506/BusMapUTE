1687d146de41c14b743c4560991f33f690f06d85
Nguyễn Khả Phương <phuongkha1506@gmail.com>
2022-05-24 12:51:43 +0700

    Update Map Fragment

--- app/src/main/java/hcmute/edu/vn/busmapute_14new/MapFragment.java
+++ app/src/main/java/hcmute/edu/vn/busmapute_14new/MapFragment.java
@@ -1,20 +1,18 @@
 package hcmute.edu.vn.busmapute_14new;
 
 import android.Manifest;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.Fragment;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
