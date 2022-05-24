4f4960784ea22ae1e2f2153cc42709f6d7e8752a
Nguyễn Khả Phương <phuongkha1506@gmail.com>
2022-05-24 12:39:47 +0700

    Update: Removed Search Activity

--- app/src/main/java/hcmute/edu/vn/busmapute_14new/MapFragment.java
+++ app/src/main/java/hcmute/edu/vn/busmapute_14new/MapFragment.java
@@ -10,11 +10,13 @@ import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -25,9 +27,20 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.RectangularBounds;
+import com.google.android.libraries.places.api.model.TypeFilter;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+
+import java.util.Arrays;
 
 public class MapFragment extends Fragment {
 
+    public static final String TAG = "MAPS: ";
+    ImageView search;
     private FusedLocationProviderClient client;
     private SupportMapFragment supportMapFragment;
 
@@ -37,13 +50,34 @@ public class MapFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_map, container, false);
 
-        // Initialize search button:
-        ImageView search = (ImageView) view.findViewById(R.id.btn_search);
-        search.setOnClickListener(new View.OnClickListener() {
+        if(!Places.isInitialized()){
+            Places.initialize(getActivity(),"AIzaSyAk2bxKJMlHto5PCM3Tputog2N4-gH1Gok");
+        }
+
+        PlacesClient placesClient = Places.createClient(getActivity());
+
+        AutocompleteSupportFragment autocompleteSupportFragment = (AutocompleteSupportFragment)
+                getChildFragmentManager().findFragmentById(R.id.autoComplete);
+
+        autocompleteSupportFragment.setTypeFilter(TypeFilter.ADDRESS);
+
+        autocompleteSupportFragment.setLocationBias(RectangularBounds.newInstance(
+                new LatLng(10.762622,106.660172),
+                new LatLng(10.762622,106.660172)
+        ));
+        autocompleteSupportFragment.setCountries("VN");
+
+        autocompleteSupportFragment.setPlaceFields(Arrays.asList(Place.Field.ID,Place.Field.NAME));
+
+        autocompleteSupportFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
-            public void onClick(View view) {
-                Intent i = new Intent(getActivity(),SearchActivity.class);
-                startActivity(i);
+            public void onError(@NonNull Status status) {
+                Log.i(TAG, "An error occured: " + status);
+            }
+
+            @Override
+            public void onPlaceSelected(@NonNull Place place) {
+                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
             }
         });
 
